013b00dbdd09a3f9bcea2cec9bae938688336372
Herpiko Dwi Aguno <herpiko@gmail.com>
2015-04-25 00:06:17 +0700

    use jszip instead of adm-zip

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 var fs = require("fs-extra");
 var async = require("async");
-var zip = require("adm-zip");
+var JSzip = require("jszip");
 var randomstring = require("randomstring");
 var concat = require("concat-files");
 var onFinished = require("on-finished");
 
+var xmlEscape = function(value) {
+  if (value) {
+    return value.replace(/&/g,"&amp;").replace(/</g,"&lt;").replace(/>/g,"&gt;");
+  }
+}
+
 var odsTemplateRowStart = "<table:table-row table:style-name='ro1'>";
 var odsTemplateRowEnd = "</table:table-row>";
 var odsTemplateCell = function(val){
-  return "<table:table-cell office:value-type='string' calcext:value-type='string'><text:p>"+val+"</text:p></table:table-cell>";
+  return "<table:table-cell office:value-type='string' calcext:value-type='string'><text:p>" + xmlEscape(val) + "</text:p></table:table-cell>";
 }
 var odsXML = "";
-
-
 
 // TODO :
 // - toBufferSync()
@@ -29,23 +33,31 @@ var convert = function(arr, path, done){
     cb();
   }, function(err){
     var tmpDir = randomstring.generate(7);
-    var templateDir = __dirname + "/template";
-    fs.copy(templateDir, __dirname + "/tmp/" + tmpDir, function(err){
-      fs.writeFile(__dirname + "/tmp/"+tmpDir+"/xml/contentRow.xml", odsXML, function(err){
+    fs.mkdir(__dirname + "/tmp/" + tmpDir, function(){
+      fs.writeFile(__dirname + "/tmp/" + tmpDir + "/contentRow.xml", odsXML, function(){
         concat([
-          __dirname + "/tmp/" + tmpDir + "/xml/contentHeader.xml",
-          __dirname + "/tmp/" + tmpDir + "/xml/contentRow.xml",
-          __dirname + "/tmp/" + tmpDir + "/xml/contentFooter.xml"
-        ], __dirname + "/tmp/" + tmpDir + "/unzipped/content.xml", function(){
+          __dirname + "/template/xml/contentHeader.xml",
+          __dirname + "/tmp/" + tmpDir + "/contentRow.xml",
+          __dirname + "/template/xml/contentFooter.xml"
+        ], __dirname + "/tmp/" + tmpDir + "/content.xml", function(){
           fs.remove(__dirname + "/tmp/" + tmpDir + "/xml", function(){
-            var zipper = new zip();
-            zipper.addLocalFolder(__dirname + "/tmp/" + tmpDir + "/unzipped");
-            var buffer =  zipper.toBuffer();
-            fs.remove(__dirname + "/tmp/" + tmpDir);
+            var zipper = new JSzip();
+            zipper.file("content.xml", fs.readFileSync(__dirname + "/tmp/" + tmpDir + "/content.xml"));
+            zipper.file("meta.xml", fs.readFileSync(__dirname + "/template/unzipped/meta.xml"));
+            zipper.file("manifest.rdf", fs.readFileSync(__dirname + "/template/unzipped/manifest.rdf"));
+            zipper.file("mimetype", fs.readFileSync(__dirname + "/template/unzipped/mimetype"));
+            zipper.file("settings.xml", fs.readFileSync(__dirname + "/template/unzipped/settings.xml"));
+            zipper.file("styles.xml", fs.readFileSync(__dirname + "/template/unzipped/styles.xml"));
+            zipper.file("META-INF/manifest.xml", fs.readFileSync(__dirname + "/template/unzipped/META-INF/manifest.xml"));
+            zipper.file("styles.xml", fs.readFileSync(__dirname + "/template/unzipped/styles.xml"));
+            zipper.file("Configurations2/accelerator/current.xml", fs.readFileSync(__dirname + "/template/unzipped/Configurations2/accelerator/current.xml"));
+            
+            var base64Str = zipper.generate({type:"base64"});
+            var buffer = new Buffer(base64Str, "base64");
             done(buffer);
+            fs.remove(__dirname + "/tmp/" + tmpDir);
           });
         })
-        if (err) return console.log(err); 
       });
     })
   })
@@ -56,6 +68,7 @@ var toBuffer = function(arr, callback){
     callback(buffer);
   });
 }
+
 module.exports = {
   toBuffer: toBuffer,
 }
